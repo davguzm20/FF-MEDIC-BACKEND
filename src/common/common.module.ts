04160cc8aa4b966e87c7eae8bd73dc85353ad5ac
davguzm20 <davguzm20@gmail.com>
2026-06-21 02:36:37 -0500

    feat(common): add filters, interceptors and common module

---
+++ src/common/common.module.ts
@@ -0,0 +1,19 @@
+import { Global, Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+  ],
+})
+export class CommonModule {}
