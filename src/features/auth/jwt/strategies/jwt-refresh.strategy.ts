6e33a90f1eb44b9d10fb66390402927e51f8c4b6
davguzm20 <davguzm20@gmail.com>
2026-06-21 03:35:06 -0500

    feat(auth): add jwt strategies, guards and decorators

---
+++ src/features/auth/jwt/strategies/jwt-refresh.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+import { envConfig } from '../../../../config/env.config';
+
+const config = envConfig();
+
+@Injectable()
+export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromBodyField('refreshToken'),
+      ignoreExpiration: false,
+      secretOrKey: config.jwtRefreshSecret,
+      passReqToCallback: true,
+    });
+  }
+
+  async validate(req: Request, payload: { sub: number; username: string; role: string }) {
+    return {
+      userId: payload.sub,
+      username: payload.username,
+      role: payload.role,
+    };
+  }
+}
