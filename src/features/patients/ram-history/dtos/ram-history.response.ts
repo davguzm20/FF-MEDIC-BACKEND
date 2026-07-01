c23a46b19fb05440784634885bc6d7c47dcb3b25
davguzm20 <davguzm20@gmail.com>
2026-07-01 01:20:28 -0500

    feat(patients): add ram history submodule

---
+++ src/features/patients/ram-history/dtos/ram-history.response.ts
@@ -0,0 +1,9 @@
+export class RamHistoryResponse {
+  ramHistoryId!: number;
+  patientId!: number;
+  activeIngredientId!: number;
+  diagnosisId!: number;
+  specifications!: string | null;
+  createdAt!: Date;
+  updatedAt!: Date;
+}
---
+++ src/features/patients/ram-history/dtos/update-ram-history.request.ts
@@ -0,0 +1,23 @@
+import { IsInt, IsString, IsOptional, Min, MaxLength } from 'class-validator';
+
+export class UpdateRamHistoryRequest {
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  patientId?: number;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  activeIngredientId?: number;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  diagnosisId?: number;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(200)
+  specifications?: string;
+}
