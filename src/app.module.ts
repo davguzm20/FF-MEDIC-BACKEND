174ce45af9a544cc3f2b745490f83ed89934bd21
davguzm20 <davguzm20@gmail.com>
2026-07-01 13:38:09 -0500

    feat(medicaments): integrate medicaments feature module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { DatabaseModule } from './database/database.module';
 import { CommonModule } from './common/common.module';
 import { AuthModule } from './features/auth/auth.module';
 import { PatientsModule } from './features/patients/patients.module';
+import { MedicamentsModule } from './features/medicaments/medicaments.module';
 
 @Module({
-  imports: [DatabaseModule, CommonModule, AuthModule, PatientsModule],
+  imports: [DatabaseModule, CommonModule, AuthModule, PatientsModule, MedicamentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
