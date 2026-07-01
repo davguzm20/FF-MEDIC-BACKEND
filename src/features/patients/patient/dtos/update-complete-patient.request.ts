70348aaad45ebf1135c313436ba60636454dbc96
davguzm20 <davguzm20@gmail.com>
2026-07-01 01:21:01 -0500

    feat(patients): integrate patients feature module

--- src/features/patients/patient/dtos/update-complete-patient.request.ts
+++ src/features/patients/patient/dtos/update-complete-patient.request.ts
@@ -1,28 +1,39 @@
 import { IsOptional, IsArray, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
 import { UpdatePatientRequest } from './update-patient.request';
+import { UpdateClinicalHistoryRequest } from '@patients/clinical-history/dtos/update-clinical-history.request';
+import { UpdateFamilyHistoryRequest } from '@patients/family-history/dtos/update-family-history.request';
+import { UpdateGynecologicalHistoryRequest } from '@patients/gynecological-history/dtos/update-gynecological-history.request';
+import { UpdateAllergyHistoryRequest } from '@patients/allergy-history/dtos/update-allergy-history.request';
+import { UpdateRamHistoryRequest } from '@patients/ram-history/dtos/update-ram-history.request';
 
 export class UpdateCompletePatientRequest extends UpdatePatientRequest {
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
-  clinicalHistories?: unknown[];
+  @Type(() => UpdateClinicalHistoryRequest)
+  clinicalHistories?: UpdateClinicalHistoryRequest[];
 
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
-  familyHistories?: unknown[];
+  @Type(() => UpdateFamilyHistoryRequest)
+  familyHistories?: UpdateFamilyHistoryRequest[];
 
   @IsOptional()
   @ValidateNested()
-  gynecologicalHistory?: unknown;
+  @Type(() => UpdateGynecologicalHistoryRequest)
+  gynecologicalHistory?: UpdateGynecologicalHistoryRequest;
 
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
-  allergyHistories?: unknown[];
+  @Type(() => UpdateAllergyHistoryRequest)
+  allergyHistories?: UpdateAllergyHistoryRequest[];
 
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
-  ramHistories?: unknown[];
+  @Type(() => UpdateRamHistoryRequest)
+  ramHistories?: UpdateRamHistoryRequest[];
 }
