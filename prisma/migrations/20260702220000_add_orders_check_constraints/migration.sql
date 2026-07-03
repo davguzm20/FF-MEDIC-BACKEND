a30b4f6374abdb17c8d4aa51978b2721ac9efcf3
davguzm20 <davguzm20@gmail.com>
2026-07-02 20:04:21 -0500

    feat(database): add orders check constraints

---
+++ prisma/migrations/20260702220000_add_orders_check_constraints/migration.sql
@@ -0,0 +1,8 @@
+-- AddCheckConstraints
+ALTER TABLE "ff_medic_db"."prescription_items" ADD CONSTRAINT "ck_prescription_items_quantity" CHECK (quantity > 0);
+
+ALTER TABLE "ff_medic_db"."referrals" ADD CONSTRAINT "ck_referrals_diagnosis_reason_exclusive" CHECK (
+    (diagnosis_id IS NOT NULL AND reason IS NULL)
+    OR
+    (diagnosis_id IS NULL AND reason IS NOT NULL)
+);
