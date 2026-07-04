25f513c0153132d761303890ea3d1952b94514f0
davguzm20 <davguzm20@gmail.com>
2026-07-04 13:56:25 -0500

    fix(patients): add validations in services of patient module

--- src/features/patients/clinical-history/clinical-history.module.ts
+++ src/features/patients/clinical-history/clinical-history.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ClinicalHistoryService } from './clinical-history.service';
 import { ClinicalHistoryRepository } from './clinical-history.repository';
+import { PatientModule } from '@patients/patient/patient.module';
+import { DiagnosisModule } from '@attentions/diagnosis/diagnosis.module';
 
 @Module({
+  imports: [PatientModule, DiagnosisModule],
   providers: [ClinicalHistoryService, ClinicalHistoryRepository],
   exports: [ClinicalHistoryService, ClinicalHistoryRepository],
 })
