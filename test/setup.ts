84eebec1a528ef485f8af3b9079e2fedeae84239
davguzm20 <davguzm20@gmail.com>
2026-07-06 13:29:16 -0500

    chore(test): fix eslint issues in setup

--- test/setup.ts
+++ test/setup.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
 process.env.DOTENV_CONFIG_QUIET = 'true';
 
 const dotenv = require('dotenv');
