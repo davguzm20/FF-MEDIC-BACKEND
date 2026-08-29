5bcf45039da4e09625c75ee8f3c6961c11d59b85
davguzm20 <davguzm20@gmail.com>
2026-08-29 00:15:29 -0500

    refactor(patients): remove activeIngredientId and diagnosisId from ram history

--- src/features/patients/ram-history/dtos/ram-history.response.ts
+++ src/features/patients/ram-history/dtos/ram-history.response.ts
@@ -1,5 +1,3 @@
 export class RamHistoryResponse {
   specifications!: string | null;
-  activeIngredient?: { name: string };
-  diagnosis?: { cie10: string; description: string };
 }
--- src/features/patients/ram-history/ram-history.module.ts
+++ src/features/patients/ram-history/ram-history.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { RamHistoryService } from './ram-history.service';
 import { RamHistoryRepository } from './ram-history.repository';
 import { PatientModule } from '@patients/patient/patient.module';
-import { ActiveIngredientModule } from '@medicaments/active-ingredient/active-ingredient.module';
-import { DiagnosisModule } from '@attentions/diagnosis/diagnosis.module';
 
 @Module({
-  imports: [PatientModule, ActiveIngredientModule, DiagnosisModule],
+  imports: [PatientModule],
   providers: [RamHistoryService, RamHistoryRepository],
   exports: [RamHistoryService, RamHistoryRepository],
 })
